336d05f85c0ef9cae2592905df43ffca284fab79
ehowey <ehowey@gmail.com>
2022-02-24 20:46:26 -0700

    Add language selector

--- next.config.js
+++ next.config.js
@@ -4,7 +4,16 @@ module.exports = {
   i18n: {
     // These are all the locales you want to support in
     // your application
-    locales: ["default", "en", "fr", "en-gb", "fr-fr", "fr-be"],
+    locales: [
+      "default",
+      "en",
+      "fr",
+      "en-gb",
+      "fr-fr",
+      "fr-be",
+      "en-ca",
+      "fr-ca",
+    ],
     // This is the default locale you want to be used when visiting
     // a non-locale prefixed path e.g. `/hello`
     defaultLocale: "default",
--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,12 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 // Import router
-// import { useRouter } from "next/router";
+import { useRouter } from "next/router";
+import LanguageSelect from "../components/LanguageSelect";
 
 export default function Home({ data }) {
   // Get locale from NextJS router
-  // We don't need this in the example but it is often useful
-  //  const { locale = "en" } = useRouter();
+  const { locale = "en" } = useRouter();
 
   // Get the page data
   const { page } = data;
@@ -24,39 +24,9 @@ export default function Home({ data }) {
         <h1 className={styles.title}>{page?.title}</h1>
 
         <p className={styles.description}>
-          Get started by editing{" "}
-          <code className={styles.code}>pages/index.js</code>
+          Current locale: <code className={styles.code}>{locale}</code>
         </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <LanguageSelect />
       </main>
 
       <footer className={styles.footer}>
@@ -90,6 +60,12 @@ export async function getStaticProps({ locale: nextLocale }) {
     },
     "en-gb": {
       title: "English - Great Britain locale",
+    },
+    "en-ca": {
+      title: "English - Canada locale",
+    },
+    "fr-ca": {
+      title: "Locale Français-Canada",
     },
     "fr-fr": {
       title: "Locale Français-France",
